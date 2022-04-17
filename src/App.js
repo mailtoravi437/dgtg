642a7001feeaeff555aa9eff5c21d0f3b07fbc3e
sayed <sayed182@gmail.com>
2022-04-17 14:46:38 +0530

    Fixed Routes

--- src/App.js
+++ src/App.js
@@ -30,17 +30,14 @@ function App() {
   return (
     <Router>
       <Routes>
-        
         <Route path="/" element={<Home/>} />
         <Route path="/about" element={<Aboutus/>} />
         <Route path="/services" element={<Services/>}/>
-        <Route exact path="/team" elements={<OurTeam/>}/>
+        <Route path="/team" element={<OurTeam/>}/>
         <Route path="/career" element={<Career/>}/>
         <Route path="/ourwork" element={<OurWork/>}/>
-        <Route path="/blog" elements={<Blog/>}/>
-        <Route path="/contactus" elements={<ContactUs/>}/>
-        
-        
+        <Route path="/blog" element={<Blog/>}/>
+        <Route path="/contactus" element={<ContactUs/>}/>
       </Routes>
     </Router>
   );
